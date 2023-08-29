eb8072f6b7051887fc380986c6b0b13af5ff4042
SpoilSaver <founder@spoilsaver.com>
2023-08-29 19:31:38 -0400

    Added internal src folder files

---
+++ src/styles/Styles.js
@@ -0,0 +1,17 @@
+// styles.js
+import { StyleSheet } from 'react-native';
+
+const commonStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
+    color: '#333',
+  },
+  // Add more common styles here
+});
+
+export default commonStyles;
